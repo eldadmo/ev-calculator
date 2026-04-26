fc64daa7533b604df94eaae4836ec768ba67b773
Eldad Michaeli <eldadmo@gmail.com>
2026-04-26 20:52:38 +0300

    Create ev-database.js

---
+++ ev-database.js
@@ -0,0 +1,53 @@
+const evDb = {
+    "vw": {
+        label: "Volkswagen",
+        models: {
+            "id4_pro": ["ID.4 / ID.5 Pro", 77],
+            "id4_pure": ["ID.4 Pure", 52],
+            "id7_pro": ["ID.7 Pro", 77],
+            "id7_s": ["ID.7 Pro S / GTX", 86],
+            "id3_pro": ["ID.3 Pro", 58]
+        }
+    },
+    "tesla": {
+        label: "Tesla",
+        models: {
+            "m3_rwd": ["Model 3 RWD", 57.5],
+            "m3_lr": ["Model 3 LR", 75],
+            "my_juniper_rwd": ["Model Y Juniper RWD", 57.5],
+            "my_juniper_lr": ["Model Y Juniper LR", 79]
+        }
+    },
+    "byd": {
+        label: "BYD",
+        models: {
+            "atto3": ["Atto 3", 60.5],
+            "dolphin": ["Dolphin", 60.5],
+            "seal": ["Seal Design", 82.5],
+            "seal_u": ["Seal U", 87]
+        }
+    },
+    "xpeng": {
+        label: "Xpeng",
+        models: {
+            "g6_std": ["G6 Standard", 65.3],
+            "g6_lr": ["G6 Long Range", 87.5],
+            "g9_std": ["G9 RWD Standard", 78.2],
+            "p7": ["P7 Wing", 82.7]
+        }
+    },
+    "hyundai": {
+        label: "Hyundai / Kia",
+        models: {
+            "ioniq5_std": ["Ioniq 5 Standard (2025+)", 63],
+            "ioniq5_ext": ["Ioniq 5 Extended (2025+)", 84],
+            "ev6": ["Kia EV6", 77.4]
+        }
+    },
+    "custom": {
+        label: "אחר",
+        models: {
+            "custom": ["הזנה ידנית", 0]
+        }
+    }
+};
